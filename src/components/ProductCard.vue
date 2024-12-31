84ed265d507aba0a5e62e3cac2b5fa687902d5b3
Albert-Valdemora <albertvaldemorat@gmail.com>
2024-12-31 13:57:20 -0400

    ProductCard V-1.1

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -7,12 +7,16 @@
  */ -->
 
 <script lang="ts" setup>
+interface Image {
+  url: string;
+}
+
 interface Product {
   id: number;
   name: string;
   category: string;
   price: number;
-  image: string;
+  image: Image;
 }
 
 defineProps<{ product: Product }>();
